35a206f58b1d2f742c3315ac4878333c7af2d517
AnthonyData99 <107485951+AnthonyData99@users.noreply.github.com>
2023-01-08 16:14:22 -0500

    Update Analisis_sentimientos_TextBlob.py

--- Analisis_sentimientos_TextBlob.py
+++ Analisis_sentimientos_TextBlob.py
@@ -1,1 +1,27 @@
 
+import pandas as pd
+import nltk
+import re
+import string
+from textblob import TextBlob
+import numpy as np
+import csv
+import time
+
+df = pd.read_csv('sentimientosDF-PedroCastill#.csv')
+##estilo textblob
+def get_polarity(text):
+    analysis=TextBlob(text)
+    if text != '':
+        try:
+                #if analysis.detect_language()=='es':
+                result=analysis.translate(from_lang='es', to='en').sentiment.polarity
+                time.sleep(5)
+                return result
+
+        except :
+                pass
+          
+          
+df['polarity'] = df['tweets_transform'].apply(get_polarity)
+df
